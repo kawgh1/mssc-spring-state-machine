ef800253d471dc90dab302e65d441f4bcb59b639
thrwwy216 <thrwwy216@tutanota.com>
2021-01-04 06:31:52 -0600

    init commit

---
+++ src/main/java/com/kwgdev/msscspringstatemachine/MsscSpringStateMachineApplication.java
@@ -0,0 +1,13 @@
+package com.kwgdev.msscspringstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscSpringStateMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscSpringStateMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwgdev/msscspringstatemachine/MsscSpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwgdev.msscspringstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSpringStateMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
